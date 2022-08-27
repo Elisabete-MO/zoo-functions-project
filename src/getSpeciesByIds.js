710bf9cc4210ed6f79a8f9d36c1bd73fdc8d89f1
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-27 18:24:08 -0300

    Primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const especies = [];
+  if (!ids) {
+    return especies;
+  }
+  ids.forEach((id) => especies.push(species.find((especie) => especie.id === id)));
+  return especies;
 }
 
 module.exports = getSpeciesByIds;
