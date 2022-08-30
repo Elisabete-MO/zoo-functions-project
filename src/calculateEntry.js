49e1d01bb0a8d2e6d2885b3e5b41818c7822c90d
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-30 17:47:46 -0300

    Oitavo requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { prices } = require('../data/zoo_data');
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const qtdObj = {
+    child: entrants.filter((crianca) => crianca.age < 18).length,
+    adult: entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50).length,
+    senior: entrants.filter((idoso) => idoso.age >= 50).length,
+  };
+  return qtdObj;
 }
 
 function calculateEntry(entrants) {
-  //  código aqui
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  const qtd = countEntrants(entrants);
+  const valorAdult = prices.adult * qtd.adult;
+  const valorChild = prices.child * qtd.child;
+  const valorSenior = prices.senior * qtd.senior;
+  return valorAdult + valorChild + valorSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
