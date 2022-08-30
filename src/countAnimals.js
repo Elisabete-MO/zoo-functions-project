5e076776ee47ca77052ff9a4734e5ce365a408db
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-29 23:25:11 -0300

    Quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const result = {};
+    species.forEach((spec) => {
+      const especies = species.find((animale) => animale.name === spec.name);
+      result[spec.name] = especies.residents.length;
+    });
+    return result;
+  }
+  const { specie: nome, sex: sexo } = animal;
+  const especieSelecionada = species.filter((especie) => especie.name === nome);
+  const [{ residents: occ }] = especieSelecionada;
+  if (!sexo) {
+    const qtd = occ.length;
+    return qtd;
+  }
+  const qtd = occ.filter((ocorrencia) => ocorrencia)
+    .filter((cont) => cont.sex === sexo).length;
+  return qtd;
 }
 
 module.exports = countAnimals;
