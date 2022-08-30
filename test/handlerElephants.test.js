e07d12ce1a6c8573ad5be802fdf752d44240de98
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-30 02:04:23 -0300

    Sexto, sétimo e décimo quinto requisitos - ajustes Lint

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,26 +1,25 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-
   it('se o tipo de HandlerElephants é função', () => {
-    expect (typeof handlerElephants).toEqual('function');   
+    expect(typeof handlerElephants).toEqual('function');
   });
 
   it('se a função não receber parametro retorna `undefined`', () => {
-    expect (handlerElephants(undefined)).toBe(undefined); 
+    expect(handlerElephants(undefined)).toBe(undefined);
   });
 
   it('se a função receber o parametro diferente de `string` retorna a mensagem `Parâmetro inválido, é necessário uma string`', () => {
-    expect (handlerElephants(12345)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants(12345)).toBe('Parâmetro inválido, é necessário uma string');
   });
 
   it('se a função receber o parametro diferente de `string` retorna a mensagem `Parâmetro inválido, é necessário uma string`', () => {
-    expect (handlerElephants(12345)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants(12345)).toBe('Parâmetro inválido, é necessário uma string');
   });
 
   it('se a função receber o parametro `count` retorna 4`', () => {
-  expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('count')).toBe(4);
   });
 
   it('se a função receber o parametro `names` retorna um array com os nomes dos animais', () => {
@@ -44,7 +43,6 @@ describe('Testes da função HandlerElephants', () => {
   });
 
   it('se a função receber parametro `availability` retorna um array de dias da semana que não contém Monday', () => {
-    expect(handlerElephants('availability')).not.toContain("Monday");
+    expect(handlerElephants('availability')).not.toContain('Monday');
   });
-
 });
